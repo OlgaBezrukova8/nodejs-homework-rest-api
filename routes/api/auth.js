4f03073859e5f9fc45fd9f3acceb6046e615969a
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-11-01 17:54:45 -0700

    Added route logout and logic for it

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -8,5 +8,6 @@ const { authenticate } = require("../../middlewares");
 router.post("/signup", ctrlWrapper(ctrl.signUp));
 router.post("/login", ctrlWrapper(ctrl.login));
 router.get("/current", authenticate, ctrlWrapper(ctrl.getCurrent));
+router.get("/logout", authenticate, ctrlWrapper(ctrl.logout));
 
 module.exports = router;

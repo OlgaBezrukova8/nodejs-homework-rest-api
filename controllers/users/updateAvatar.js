5cb49fffb25a8ea80306f2333cd6b62c45b101ad
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-11-07 18:41:35 -0800

    Added update avatars method

---
+++ controllers/users/updateAvatar.js
@@ -0,0 +1,23 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { User } = require("../../models/user");
+
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+
+  const extension = originalname.split(".").pop();
+  const filename = `${_id}.${extension}`;
+  const resultUpload = path.join(avatarsDir, filename);
+
+  await fs.rename(tempUpload, resultUpload);
+  const avatarUrl = path.join("avatars", filename);
+  await User.findByIdAndUpdate(_id, { avatarUrl });
+  res.json({
+    avatarUrl,
+  });
+};
+
+module.exports = updateAvatar;
